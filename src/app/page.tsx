b5e2b9c8e48c449d61dd6d50dc3978ce1c97846b
Fujiyama722 <s.fujiyama722@gmail.com>
2025-07-26 09:31:14 +0900

    ✨ useEffectフックを追加し、マウント時に背景色を変更する処理を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,25 @@
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 import { Footer } from "@/components/Footer";
-import { useCallback } from "react";
-
-const foo = 1;
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
+
+  const foo = 1;
   const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => { //useCallback()で関数(e)=>{}を囲う
     console.log(e.currentTarget.href);
     e.preventDefault();
     alert(foo);
   }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = 'skyblue';
+    // ↑マウント時の処理
+    // ↓アンマウント時の処理
+    return () => {
+      document.body.style.backgroundColor = '';
+    }
+  },[])
 
   return (
     <div className="font-sans flex flex-col justify-around items-center justify-items-center min-h-screen p-4 pb-20 gap-1 sm:p-10">
