4892baade274a2f9b34a4dde3233e1c1491cdf0e
Fujiyama722 <s.fujiyama722@gmail.com>
2025-07-26 23:02:43 +0900

    ✨ useStateフックを追加し、ボタンクリックでカウンターを増加させる機能を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,33 @@
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 import { Footer } from "@/components/Footer";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [foo, setFoo] = useState(1);
 
-  const foo = 1;
-  const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => { //useCallback()で関数(e)=>{}を囲う
-    console.log(e.currentTarget.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setFoo(foo => foo + 1)
+  };
 
   useEffect(() => {
-    document.body.style.backgroundColor = 'skyblue';
+    document.body.style.backgroundColor = "skyblue";
     // ↑マウント時の処理
     // ↓アンマウント時の処理
     return () => {
-      document.body.style.backgroundColor = '';
-    }
-  },[])
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className="font-sans flex flex-col justify-around items-center justify-items-center min-h-screen p-4 pb-20 gap-1 sm:p-10">
       {/* スタイルが面倒ならこの<div>すらもコンポーネント化してしまおう */}
       <Header />
       <Main title="index" />
-      <a
-        href="./about/"
-        className=" text-white bg-blue-700 "
-        onClick={handleClick}
-      >
+      <h1>{foo}</h1>
+      <button className=" text-white bg-blue-700 " onClick={handleClick}>
         ボタン
-      </a>
+      </button>
       <Footer />
     </div>
   );
