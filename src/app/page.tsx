2524dca97223a0aaa85a4384ac3b605125d5ece6
Fujiyama722 <s.fujiyama722@gmail.com>
2025-07-28 07:19:52 +0900

    ✨ useStateフックを追加し、文字列と真偽値について解説（テキスト入力と表示切替機能を実装）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,30 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   // 講座12回目参照
   const handleClick = useCallback(() => {
     console.log(count);
     if (count < 10) {
-      setCount((count) => count + 1) // {count + 1}これだめ  {(count) => count + 1}これ正解
+      setCount((count) => count + 1); // {count + 1}これだめ  {(count) => count + 1}これ正解
     }
   }, [count]);
 
-  // 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 5) {
+      return alert("5文字以内にしてください");
+    }
+    // onChenge で入力テキストに変更
+    setText(e.target.value.trim());
+  }, []);
+
+  //
   useEffect(() => {
     console.log("foo");
     document.body.style.backgroundColor = "skyblue";
@@ -27,15 +41,25 @@ export default function Home() {
   }, [count]);
   // []に変数を入れると、変更されたタイミングでuseEffectの処理が走る
   // 配列なのでいくつでも変数を入れられる、どれかしらに変更があった時点で処理が走る、これで制御可能
+
   return (
     <div className="font-sans flex flex-col justify-around items-center justify-items-center min-h-screen p-4 pb-20 gap-1 sm:p-10">
       {/* スタイルが面倒ならこの<div>すらもコンポーネント化してしまおう */}
       <Header />
+      <div className="flex flex-col gap-2 items-center justify-center">
+        {isShow ? <h1>{count}</h1> : null}
+        <button className=" text-white bg-blue-700 " onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+        {/* <label htmlFor="text-input">テキストボックス</label> */}
+        <input
+          id="text-input"
+          type="text"
+          value={text}
+          className="bg-white border text-black "
+          onChange={handleChange}
+        />
+      </div>
       <Main title="index" />
-      <h1>{count}</h1>
-      <button className=" text-white bg-blue-700 " onClick={handleClick}>
-        ボタン
-      </button>
       <Footer />
     </div>
   );
